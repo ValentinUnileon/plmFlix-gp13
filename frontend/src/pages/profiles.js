7f5d10d99744477047bd15873d7d13350651fc82
ValentinUnileon <74121671+ValentinUnileon@users.noreply.github.com>
2023-05-04 15:08:55 +0200

    login valentin

--- frontend/src/pages/profiles.js
+++ frontend/src/pages/profiles.js
@@ -1,7 +1,14 @@
-import * as React from 'react';
+import * as React from 'react'
+import { Box, Avatar, Icons } from 'grommet';
+
 
 export default function Profiles() {
     return (
-        <p>hola</p>
+        <Box direction="row"  gap="small">
+            <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />   
+            <Avatar background="brand">
+                <Icons.UserFemale color="text-strong" />
+            </Avatar>
+        </Box>
     )
 }

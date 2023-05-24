3d3a70f77121074a19f351fcbbb26108c5466b0d
ValentinUnileon <vsantp01@estudiantes.unileon.es>
2023-05-24 17:17:35 +0200

    e

--- frontend/src/pages/const/const.js
+++ frontend/src/pages/const/const.js
@@ -1,4 +1,4 @@
 export function getEndpoint(resource) {
-    //return `/api${resource}`;
-    return `http://localhost:5000/api${resource}`;
+    return `/api${resource}`;
+    //return `http://localhost:5000/api${resource}`;
 }

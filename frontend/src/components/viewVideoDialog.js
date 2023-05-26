f32670a9f8c1d2905e1b86cd11a50606b0bcba0f
Weptzel <72155672+Weptzel@users.noreply.github.com>
2023-05-26 21:55:16 +0200

    solo por no perderlo xd

--- frontend/src/components/viewVideoDialog.js
+++ frontend/src/components/viewVideoDialog.js
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Header from "../components/header";
-import Banner from "../components/banner";
-import Footer from "../components/footer";
 import Categoria from "../components/category";
 import ReactPlayer from "react-player";
-
 import {Button} from "@mui/material";
+import '../cssComponents/viewVideo.css';
+import icon from "../images/flecha-correcta.png";
+import { useState } from "react";
+import { useRef } from "react";
 const PREFIX = "viewFilms";
 
 const classes = {
@@ -16,7 +16,7 @@ const classes = {
 
 const Root = styled("div")({
   [`&.${classes.root}`]: {
-    backgroundColor: "#111",
+    backgroundColor: "white",
     width: "100%",
     height: "100%",
   },
@@ -24,24 +24,103 @@ const Root = styled("div")({
   [`& .${classes.container}`]: {
     margin: "10px 10px 10px 10px",
   },
+
+  button: {
+    height: '75px',
+    width: '75px',
+    left: '50px',
+    top: '125px',
+    cursor: 'pointer',
+    color: '#fff',
+    fontWeight: 700,
+    borderRadius: '50%', 
+    backGroundColor: 'rgba(51,51,51,0.5)',
+    display: 'block',
+    transform: 'rotate(90deg)',
+    }, 
+    "& button:hover": {
+      color: 'red',
+      backGroundColor: 'blue',
+    },  
+
 });
 
 export default function ViewFilms({user, profiles, videoURL, videoID}) {
-    
+  const [duration, setDuration] = useState(0);
+  const [SecondsViews, setSecondsViews] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [isReady, setIsReady] = useState(false);
+  const playerRef = useRef();
+
+  const handleDuration = (videoDuration) => {
+    setDuration(videoDuration);
+  }
+  
+  /* Calcular el tiempo restante de video */
+  const handleProgress = (progress) => {
+    //const vidDuration = duration;
+    const currentTime = progress.playedSeconds;
+    // remainingTime = vidDuration - currentTime;
+    setSecondsViews(currentTime);
+  };
+
+   const onReady = React.useCallback(() => {
+    if(!isReady){
+      /* Duracion - tiempo Restante */
+      const timeStart = 40;
+      playerRef.current.seekTo(timeStart, "seconds");
+    }
+  }, [isReady])
+
+  /* onClick() 
+  guarda los sec remaining de cada video de cada usuario
+  y me regresa a home
+  usar profile y videoID para ello 
+  */
+
+  
   return (
     <Root className={classes.root}>
       <div className="body">
         <div className="video-back">
           <ReactPlayer
-            url= {videoURL} 
+            ref = {playerRef}
+            url={videoURL} 
             controls
-            playing
-            muted
-            width="100%"
-            height="100%"
+            playing = {isPlaying}
+            onDuration={handleDuration}
+            onProgress={handleProgress}
+            onReady={onReady}
+            width="50%"
+            height="50%"
           />
-          <Button></Button>
         </div>
+        <div className="button">
+            <Button>
+              <img src={icon} height="50px" width="50px" />
+              {/*
+              funcion onClick();
+              */ }
+            </Button>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+        <p color="green"> duracion del video: {duration} segundos</p>
+        <p color="green"> Has visto del video: {SecondsViews} segundos</p>
       </div>
     </Root>
   );

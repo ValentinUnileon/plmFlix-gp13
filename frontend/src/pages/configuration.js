01d96feb074d109c5c285e8e1be0227ea7c3924d
ValentinUnileon <74121671+ValentinUnileon@users.noreply.github.com>
2023-05-26 14:56:11 +0200

    avances

--- frontend/src/pages/configuration.js
+++ frontend/src/pages/configuration.js
@@ -3,8 +3,10 @@ import { styled } from '@mui/material/styles';
 import {Button, TextField, Typography, Alert, Box} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useState } from "react";
 import {getEndpoint} from './const/const';
 import { Divider } from '@mui/material';
+import Profile from "../components/profile";
 
 const PREFIX = 'configuration';
 
@@ -12,7 +14,8 @@ const classes = {
   root: `${PREFIX}-root`,
   textField: `${PREFIX}-textField`,
   formContainer: `${PREFIX}-formContainer`,
-  textoPrincipal: `${PREFIX}-textoPrincipal`
+  textoPrincipal: `${PREFIX}-textoPrincipal`,
+  perfiles:`${PREFIX}-perfiles`
 };
 
 const Root = styled('div')({
@@ -20,7 +23,8 @@ const Root = styled('div')({
     //backgroundImage: 'url(https://www.ribescasals.com/media/catalog/product/cache/ac95d467f39086acf44821b87fe7ae41/t/e/tela-loneta-negra.jpg)',
     backgroundSize: 'cover',
     backgroundColor: '#111',
-    height: '100vh',
+    height: '100%',
+
     display: 'flex',
     justifyContent: 'center',
     alignContent: 'center'
@@ -39,22 +43,22 @@ const Root = styled('div')({
     },
   },
   [`& .${classes.formContainer}`]: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: 'rgba(0, 0, 0, 0.88)',
-    padding: '2rem',
-    borderRadius: '0.5rem',
-    boxShadow: '0 0.5rem 1rem rgba(0, 0, 0, 0.9)',
-    marginTop: '0.4%'
+ 
+
 
   },
   [`& .${classes.textoPrincipal}`]: {
     
     marginTop: '10px',
     fontFamily: 'Verdana'
-  }
+  },
+  [`& .${classes.perfiles}`]: {
+    
+    display: 'flex',
+    justifyContent: 'center',
+    
+  },
+
 });
 
 export default function Configuration() {
@@ -63,14 +67,17 @@ export default function Configuration() {
 
   const navigate = useNavigate();
 
+  const profile ={
+
+    name: "hola",
+    
+  }
+  const [profilesList, setProfileList] = useState([profile, profile, profile ]);
+
 
     
   const handleSubmit = (event) => {
     event.preventDefault();
-   
-
-
-
   };
 
   const actualizarNombre = () => {
@@ -82,15 +89,21 @@ export default function Configuration() {
 
     console.log('Se hizo clic en el botón');
   };
+  function borrar(profile){
+    console.log("borrando")
+  }
+
+
 
   return (
     <Root className={classes.root}>
 
-        <Typography variant="h4" frontWeight = 'bold' color='white' style={{ fontFamily: 'Palatino', marginTop: '12px' }} className={classes.textoPrincipal}>Configuración de usuario</Typography>
-        <br></br><br></br>
 
 
         <Box component="form" onSubmit={handleSubmit} noValidate className={classes.formContainer}>
+          
+        <Typography variant="h4" frontWeight = 'bold' color='white' style={{ fontFamily: 'Palatino', marginTop: '12px' }} className={classes.textoPrincipal}>Configuración de usuario</Typography>
+        <br></br><br></br>
           <Typography variant="h5" frontWeight = 'bold' style={{ fontFamily: 'Palatino' }} color='white'>Cambiar nombre de usuario</Typography>
           <TextField label='Nuevo nombre' className={classes.textField} margin="normal" fullWidth id="email" name="email" autoComplete="email"  autoFocus/>
           <Button variant="contained" color="primary" fullWidth sx={{ width: '200px' }} onClick={actualizarNombre} type="submit">Cambiar nombre</Button>
@@ -107,6 +120,27 @@ export default function Configuration() {
           <br></br><br></br>
           <Divider color="grey" variant="fullWidth" />
           <br></br><br></br>
+
+          <Typography variant="h5" frontWeight = 'bold' style={{ fontFamily: 'Palatino' }} color='white'>Eliminar perfil</Typography>
+
+          <br></br><br></br>
+          {profilesList.map((profile , index) => (
+
+                <div className={classes.perfiles}>
+
+                  <Profile
+                    profile={profile}
+                    click={borrar} />
+
+                   <div className={classes.a}> 
+                   <button>asdf</button>  
+                   </div>
+
+                </div>
+
+
+
+          ))}
 
           <Button variant="contained" color="primary" fullWidth type="submit">Editar perfiles</Button>
         </Box>

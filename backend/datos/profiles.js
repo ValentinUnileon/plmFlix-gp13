d23bea5cd367cc0325e755e5cbf61d590b4efdb3
ValentinUnileon <vsantp01@estudiantes.unileon.es>
2023-05-30 22:05:05 +0200

    cambios

--- backend/datos/profiles.js
+++ backend/datos/profiles.js
@@ -2,9 +2,9 @@
 
 var data = [
     {name: "ppaner", user: "0"},
-    {name: "ali", user: "0"},
-    {name: "La Otaku", user: "1"},
-    {name: "illo", user: "1"},
+    {name: "vsantp", user: "0"},
+    {name: "paula", user: "1"},
+    {name: "maria", user: "1"},
     {name: "paco", user: "1"},
     {name: "edu", user: "2"}
 ];
